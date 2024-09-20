71ff0d9bd09315ab882a809597319d7f4147b602
Michał Przytarski <108720084+michal-przytarski@users.noreply.github.com>
2024-09-20 10:33:27 +0200

    Revert "Fix proxy for image auto (#466)"
    This reverts commit 09c8bd7865dfbcd17c458ed12e71f6d0714e3d44.

--- main.go
+++ main.go
@@ -7,6 +7,8 @@ import (
 	"encoding/json"
 	"fmt"
 	"io"
+	"net/http"
+	"net/url"
 	"os"
 	"os/signal"
 	"path"
@@ -294,7 +296,14 @@ The commands work as following:
 		return
 	}
 	proxyUrl, _ := arguments.String("--proxyurl")
-	exitIfError("could not parse proxy url", ios.UseHttpProxy(proxyUrl))
+	if proxyUrl == "" {
+		proxyUrl = os.Getenv("HTTP_PROXY")
+	}
+	if proxyUrl != "" {
+		parsedUrl, err := url.Parse(proxyUrl)
+		exitIfError("failed parsing proxy url", err)
+		http.DefaultTransport = &http.Transport{Proxy: http.ProxyURL(parsedUrl)}
+	}
 
 	b, _ := arguments.Bool("listen")
 	if b {
